ee4ad3b85f323e89bf9cc5d7a886dfb09ffcf4bc
benfehr <benjamin_louis_foote@hotmail.com>
2024-08-10 09:40:18 +0000

    create migrations using models.py

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+    User, on_delete=models.CASCADE, related_name="blog_posts", default="foo"
 )
 content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
+created_on = models.DateTimeField(auto_now_add=True, null='true')
 status = models.IntegerField(choices=STATUS, default=0)
 excerpt = models.TextField(blank=True)
+updated_on = models.DateTimeField(auto_now=True)
+
+def foo():
+    return 'bar'
